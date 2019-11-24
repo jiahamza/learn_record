d943b9437ddb021ca9621aa14fa9451660ee8e1c
jiahamza <438564935@qq.com>
2019-11-24 11:29:54 +0800

    集合类addAll(null)会报错

--- src/main/java/base/Test.java
+++ src/main/java/base/Test.java
@@ -1,7 +1,18 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        //System.out.println("hello world!");
+        testAddAll();
+    }
+
+    private static void testAddAll() {
+        List list = new ArrayList();
+        //报错
+        list.addAll(null);
+
     }
 }
